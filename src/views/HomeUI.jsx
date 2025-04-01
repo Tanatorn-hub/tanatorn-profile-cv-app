403cb7e1f74987b2aadef0c9ad5b771ad5776bd6
Tanatorn-hub <jaysiam46@gmail.com>
2025-04-01 17:12:21 +0700

    เสร็จ app bar กำลังเริ่มส่วนของ profile

--- src/views/HomeUI.jsx
+++ src/views/HomeUI.jsx
@@ -1,10 +1,48 @@
+import { AppBar, Container, Toolbar, Box, Typography, Avatar } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import pic1 from '../assets/pic1.jpg'
 
 function HomeUI() {
   return (
     <>
-        HomeUI
-        สวัสดี!!
+    <Container maxWidth="xl" sx={{bgcolor:'#121212'}}>
+
+      {/* Nav bar */}
+      <AppBar sx={{backgroundColor:'#121212'}} position='static'>
+        <Toolbar>
+          <Typography variant='h5' sx={{ flexGrow: 1, fontWeight:'bold'}}>
+            Tanatorn Phloenmalai
+          </Typography>
+          <Typography component={Link} to='/work' sx={{textDecoration:'none', color:'white', mr:'10px'}}>
+            Work
+          </Typography>
+          <Typography component={Link} to='/About' sx={{textDecoration:'none', color:'white', mr:'10px'}}>
+            About
+          </Typography>
+          <Typography component={Link} to='/Contact' sx={{textDecoration:'none', color:'white', mr:'10px'}}>
+            Contact
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      {/* Profile */}
+      <Box id={'profile'} sx={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+        <Typography variant='h2' sx={{color:'white', mt:'50px', ml:'50px',textAlign:'left', fontWeight:'normal', fontSize:'100px'}}>
+          HI, I AM
+        </Typography><br/>
+        
+        <Avatar 
+        src={pic1} 
+        variant='rounded' 
+        sx={{width:'400px', height:'400px',mt:'50px'}}/>
+        
+
+      </Box>
+      
+
+    </Container>
+
     </>
   )
 }
